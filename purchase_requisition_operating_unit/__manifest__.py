a574721224e462472f47aa47ca53436e8a1044f8
Que Nguyen <quenguyen@tekniq.com.vn>
2022-09-08 21:21:34 -0700

    manifest 13.0

--- purchase_requisition_operating_unit/__manifest__.py
+++ purchase_requisition_operating_unit/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Operating Unit in Purchase Requisitions",
-    "version": "14.0.1.0.1",
+    "version": "13.0.1.0.1",
     "author": "ForgeFlow S.L.,"
     "Serpent Consulting Services Pvt. Ltd.,"
     "Odoo Community Association (OCA)",
